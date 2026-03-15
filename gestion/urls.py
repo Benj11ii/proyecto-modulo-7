8f1bc9d583aaeae29cf1916dad6ba11f54da38d6
Benj11ii <benjaminalonsocarmona@gmail.com>
2026-03-15 15:43:35 -0300

    visualización de codigo para pág. web

--- gestion/urls.py
+++ gestion/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ListaClientesView
+from .views import ListaClientesView,  CrearClienteView, EditarClienteView
 
 urlpatterns = [
     # Definimos la ruta para la lista de clientes
     path('clientes/', ListaClientesView.as_view(), name='lista_clientes'),
+    path('clientes/nuevo/', CrearClienteView.as_view(), name='crear_cliente'),
+      path('clientes/editar/<int:pk>/', EditarClienteView.as_view(), name='editar_cliente'),
 ]
--- gestion/views.py
+++ gestion/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView
+from django.urls import reverse_lazy
 from .models import Cliente
 
 # Esta vista se encarga de traer todos los clientes de la BD y pasarlos al HTML
@@ -6,3 +7,15 @@ class ListaClientesView(ListView):
     model = Cliente
     template_name = 'gestion/lista_clientes.html'
     context_object_name = 'clientes'
+
+class CrearClienteView(CreateView):
+    model = Cliente
+    fields = ['nombre', 'email', 'telefono']
+    template_name = 'gestion/cliente_form.html'
+    success_url = reverse_lazy('lista_clientes')
+
+class EditarClienteView(UpdateView):
+    model = Cliente
+    fields = ['nombre', 'email', 'telefono']
+    template_name = 'gestion/cliente_form.html'
+    success_url = reverse_lazy('lista_clientes')
