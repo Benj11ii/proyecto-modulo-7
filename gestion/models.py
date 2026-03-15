c7583268276d6db016ea424647797958e37fd3e5
Benj11ii <benjaminalonsocarmona@gmail.com>
2026-03-15 14:40:40 -0300

    preparación de app billetera virtual

--- gestion/models.py
+++ gestion/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField(unique=True) # unique=True es una validación
+    telefono = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Cuenta(models.Model):
+    # Relación Uno a Uno: Un cliente tiene una sola cuenta billetera principal
+    cliente = models.OneToOneField(Cliente, on_delete=models.CASCADE)
+    saldo = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Cuenta de {self.cliente.nombre} - Saldo: ${self.saldo}"
+
+class Transaccion(models.Model):
+    # Opciones para el campo 'tipo'
+    TIPO_CHOICES = [
+        ('ingreso', 'Ingreso'),
+        ('egreso', 'Egreso'),
+    ]
+    
+    # Relación Muchos a Uno: Una cuenta puede tener muchas transacciones
+    cuenta = models.ForeignKey(Cuenta, on_delete=models.CASCADE, related_name='transacciones')
+    tipo = models.CharField(max_length=10, choices=TIPO_CHOICES)
+    monto = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.get_tipo_display()} de ${self.monto} en cuenta de {self.cuenta.cliente.nombre}"
